f773a790e57421041d8ae1cbf2934aa3086cdc26
spacerose36 <mariapaulinavilla11a@gmail.com>
2023-08-23 21:38:48 -0500

    Better description

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+    response.json({ info: 'Planets and stars API' })
 })
 
 app.get('/stars', db.getStars)
