6397db34fce2a873220a3d60d3d6375805105a4d
spacerose36 <mariapaulinavilla11a@gmail.com>
2023-08-23 21:20:45 -0500

    Improvements to use Dockerfile instead of compose

--- queries.js
+++ queries.js
@@ -2,10 +2,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
-  host: 'host.docker.internal',
-  database: 'postgres',
-  password: '0123456',
-  port: 5432,
+  host: 'containers-us-west-207.railway.app',
+  database: 'railway',
+  password: 'g7vWnVKvF2Qe3wX1ZA55',
+  port: 7012,
 })
 
 const getStars = (request, response) => {
