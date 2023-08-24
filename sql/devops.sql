0b9717092d30fe8e764f83bc6e2a666506c20396
spacerose36 <mariapaulinavilla11a@gmail.com>
2023-08-23 23:56:38 -0500

    remove unnecessary folder

--- sql/devops.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE stars(star_name VARCHAR(32) NOT NULL, mass INTEGER, PRIMARY KEY (star_name));
-CREATE TABLE planets(planet_name VARCHAR(32) NOT NULL, mass INTEGER,  PRIMARY KEY (planet_name), FOREIGN KEY (star_name) REFERENCES stars(star_name));
